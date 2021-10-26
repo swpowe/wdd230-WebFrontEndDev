23507be7ac77a7378997306c102f854f11965e82
Spencer Powell <spencer@alegralearning.com>
2021-10-25 18:33:47 -0600

    working on gallery

---
+++ lesson7/js/visit.js
@@ -0,0 +1,24 @@
+const visit = document.getElementById('visit')
+
+let previous = new Date("09/25/2021");
+let today = new Date("10/25/2021");
+
+// localStorage.previous = previous;
+// localStorage.today = today;
+
+// localStorage.previous = '';
+
+// console.log(`Today:${today.getMonth() + 1}, ${today.getDate()}, ${today.getFullYear()}`)
+
+function difference(date1, date2) {  
+    const date1utc = Date.UTC(date1.getFullYear(), date1.getMonth(), date1.getDate());
+    const date2utc = Date.UTC(date2.getFullYear(), date2.getMonth(), date2.getDate());
+      day = 1000*60*60*24;
+    return(date2utc - date1utc)/day
+  }
+  
+  let date1 = new Date("2021-09-25")
+        let date2 = new Date("2021-10-25")
+        let time_difference = difference(date1,date2)
+  
+  console.log(time_difference)
