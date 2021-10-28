1c04101220ddb810447c6930aebade8d2f2fb95b
Spencer Powell <spencer@alegralearning.com>
2021-10-27 21:06:29 -0600

    adjusted large image hover box shadow

--- lesson7/js/visit.js
+++ lesson7/js/visit.js
@@ -13,7 +13,7 @@ let today = new Date("10/25/2021");
 function difference(date1, date2) {  
     const date1utc = Date.UTC(date1.getFullYear(), date1.getMonth(), date1.getDate());
     const date2utc = Date.UTC(date2.getFullYear(), date2.getMonth(), date2.getDate());
-      day = 1000*60*60*24;
+    let day = 1000*60*60*24;
     return(date2utc - date1utc)/day
   }
   
