c48c8e7ad79ee76a4646e841346f1919c3558c96
Spencer Powell <spencer@alegralearning.com>
2021-11-02 21:44:45 -0600

    added storm center page

---
+++ week8/js/visit.js
@@ -0,0 +1,31 @@
+const visit = document.getElementById('visit');
+
+let previous = new Date();
+let today = new Date()
+let todayF = new Date(`${today.getMonth() + 1}/${today.getDate()}/${today.getFullYear()}`);
+let localPrevious = localStorage.getItem('previous');
+console.log(`Local Previous is: ${localPrevious}.`)
+
+
+if(localPrevious === '' || localPrevious === null) {
+  console.log('previous is empty.')
+  // let previous = new Date();
+  previous = todayF;
+  // previous = new Date("09/25/2021"); // !! Testing ONLY
+}else if(isNaN(typeof(previous))) {
+  console.log("previous is NAN");
+  previous = todayF;
+}
+else {
+  previous = new Date(localPrevious);
+  console.log(typeof(previous), typeof(todayF))
+}
+
+let diffTime = todayF.getTime() - previous.getTime();
+let difftodayF = diffTime / (1000 * 3600 * 24);
+console.log(`todayF since last visit: ${difftodayF}`)
+
+localStorage.setItem('previousVisit', todayF);
+console.log(`Local Previous set to: ${todayF}`);
+
+visit.innerText = difftodayF
