2608777e67157957bc314ca4852aeecbbf54346b
Spencer Powell <spencer@alegralearning.com>
2021-12-14 22:22:01 -0700

    progress on homepage

--- week12/final_project/js/homepage-script.js
+++ week12/final_project/js/homepage-script.js
@@ -19,153 +19,65 @@ function toggleMenu() {
     document.getElementById("primary-nav").classList.toggle("hide");
 }
 
-
-// id="forecast-current-day"
-
-/* <div class="content">
-<p>Current Temp: <span id="current-temp"></span></p>
-<p>Current Condition Description: <span id="current-condition"></span></p>
-<p>Humidity: <span id="current-humidity"></span></p>
-<!-- Weather Alert that's closable popup? -->
-<!-- Display and Label a 3 day forcast of the "day" temp from the current date -->
-<div class="weather-forecast">
-  <p>3 Day forecast for <span id="forecast-current-day">Monday Dec 12</span></p>
-  <ul class="three-day-forecast">
-    <li>
-      <img src="https://via.placeholder.com/150"/>
-      <p id="day1">Monday</p>
-      <p id="day1-temp">98</p>
-    </li>
-    <li>
-      <img src="https://via.placeholder.com/150"/>
-      <p id="day1">Monday</p>
-      <p id="day2-temp">98</p>
-    </li>
-    <li>
-      <img src="https://via.placeholder.com/150"/>
-      <p id="day1">Monday</p>
-      <p id="day3-temp">98</p> */
-
-
-
 // >>>>>>>>>>>>>>>>>>>>>>>>>.
-const apiURL = "https://api.openweathermap.org/data/2.5/";
+
+let page = window.location.pathname;
+// const apiURL = "https://api.openweathermap.org/data/2.5/";
+const apiURL = "https://api.openweathermap.org/data/2.5/onecall?";
 const apiKEY = "c9d68fbf2ae49e0738af3fb4229133ea";
 let cityId = 5781860;
-page = window.location.pathname;
+let lon = -111.654922
+let lat = 40.11496;
+// console.log(`CityId is: ${cityId}`);
 
-// if(page.includes("fish")) {
-//   cityId = 5585000;
-// }else if(page.includes('preston')) {
-//   cityId = 5604473;
-// }else if(page.includes('soda')) {
-//   cityId = 5607916;
-// }
-
-console.log(`CityId is: ${cityId}`);
-
-let urlWeather = `${apiURL}weather?id=${cityId}&appid=${apiKEY}&units=imperial`;
-let urlForecast = `${apiURL}forecast?id=${cityId}&appid=${apiKEY}&units=imperial`;
+// let urlWeather = `${apiURL}weather?id=${cityId}&appid=${apiKEY}&units=imperial`;
+// let urlForecast = `${apiURL}forecast?id=${cityId}&appid=${apiKEY}&units=imperial`;
+let urlWeather = `${apiURL}lat=${lat}&lon=${lon}&units=imperial&appid=${apiKEY}`;
 
 fetch(urlWeather)
   .then((response) => response.json())
   .then((jsObject) => {
     console.log(jsObject);
-    let descSplit = jsObject.weather[0].description.split(" ");
-    let phrase = descSplit
-      .map((word) => {
-        return word[0].toUpperCase() + word.substring(1);
-      })
-      .join(" ");
+    // let descSplit = jsObject.weather[0].description.split(" ");
+    // let phrase = descSplit
+    //   .map((word) => {
+    //     return word[0].toUpperCase() + word.substring(1);
+    //   })
+    //   .join(" ");
+
     // Summary Spans
     // document.getElementById(
-    //   "current-value"
+    //   "current-temp"
     // ).textContent = ` ${phrase} ${Math.round(jsObject.main.temp)}\xB0`;
-    // document.getElementById("high-value").textContent = Math.round(
-    //   jsObject.main.temp_max
-    // );
-    // document.getElementById("humidity-value").textContent =
+    // document.getElementById("current-condition").textContent =
+    //   jsObject.weather[0].description;
+    // document.getElementById("current-humidity").textContent =
     //   jsObject.main.humidity;
-    // document.getElementById("wind-value").textContent = Math.round(
-    //   jsObject.wind.speed
-    // );
+    let today = new Date();
+    // const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    // let month = months[today.getMonth()];
+    // let week = days[today.getDay()];
+    let day = today.getDate();
+
+    // let timestamp = `${week} ${month} ${day}`;
+
+    // document.getElementById('forecast-current-day').textContent = timestamp;
+    document.getElementById('current-temp').textContent = `${Math.round(jsObject.current.temp)}\xB0`;
+    document.getElementById('current-condition').textContent = jsObject.current.weather[0].description;
+    document.getElementById('current-humidity').textContent = jsObject.current.humidity;
+
+    // 3 day forecast
+    let imgRoot = "https://openweathermap.org/img/w/";
+    document.getElementById('day1-img').src = `${imgRoot}${jsObject.daily[0].weather[0].icon}.png`;
+    document.getElementById('day2-img').src = `${imgRoot}${jsObject.daily[1].weather[0].icon}.png`;
+    document.getElementById('day3-img').src = `${imgRoot}${jsObject.daily[2].weather[0].icon}.png`;
+
+    document.getElementById('day1').textContent = (days[today.getDay() + 1]).substring(0,3);
+    document.getElementById('day2').textContent = (days[today.getDay() + 2]).substring(0,3);
+    document.getElementById('day3').textContent = (days[today.getDay() + 3]).substring(0,3);
+
+    document.getElementById('day1-temp').textContent = `${Math.round(jsObject.daily[0].temp.day)}\xB0`;
+    document.getElementById('day2-temp').textContent = `${Math.round(jsObject.daily[1].temp.day)}\xB0`;
+    document.getElementById('day3-temp').textContent = `${Math.round(jsObject.daily[3].temp.day)}\xB0`;
   });
-
-// fetch(urlForecast)
-//   .then((response) => response.json())
-//   .then((forecastObj) => {
-//     // get forcast data
-//     // go through forecast.list and search for 18:00:00
-//     // console.log(forecastObj);
-//     const result = forecastObj.list.filter((item) => {
-//       // console.log(item.dt_txt)
-//       return item.dt_txt.includes("18:00:00");
-//     });
-//     // console.log(result);
-
-//     // Swith out table Icons
-//     for (let i = 0; i < 5; i++) {
-//       const imagesrc =
-//         "https://openweathermap.org/img/w/" +
-//         result[i].weather[0].icon +
-//         ".png"; // note the concatenation
-//       const desc = result[i].weather[0].description; // note how we reference the weather array
-//       document.getElementById(`forecast-table-day${i+1}-img`).textContent = imagesrc; // informational specification only
-//       document
-//         .getElementById(`forecast-table-day${i+1}-img`)
-//         .setAttribute("src", imagesrc); // focus on the setAttribute() method
-//       document
-//         .getElementById(`forecast-table-day${i+1}-img`)
-//         .setAttribute("alt", desc);
-
-//       // Table Spans and img
-//       document.getElementById(
-//         `forecast-table-day${i+1}-temp`
-//       ).textContent = `${Math.round(result[i].main.temp)}\xB0`;
-//     }
-//   });
-
-
-// #region EVENTS EVENTS EVENTS
-
-// fetch("js/companyData.json")
-//   .then((res) => res.json())
-//   .then((data) => {
-//     console.log(data);
-
-//     // get main div ID to append everything to
-//     // div for each company
-//     // icon div at same level
-//     let main = document.getElementById('events-main');
-
-//     for (let i = 0; i < data.companies.length; i++) {
-//         let logo = document.createElement('img');
-//         let name = document.createElement('h2');
-//         let bio = document.createElement('p');
-//         let address = document.createElement('p');
-//         let url = document.createElement('p');
-//         let a = document.createElement('a');
-
-//         logo.src = data.companies[i].logo;
-//         name.innerText = data.companies[i].companyName;
-//         bio.innerText = data.companies[i].bio;
-//         address.innerText = data.companies[i].address;
-//         a.href = data.companies[i].url;
-//         a.innerText = data.companies[i].url;
-//         a.target = "_blank";
-
-//         url.appendChild(a);
-
-//         main.appendChild(logo);
-//         main.appendChild(name);
-//         main.appendChild(bio);
-//         main.appendChild(address);
-//         main.appendChild(url);
-//     }
-
-
-//   });
-
-
-
-// #endregion
